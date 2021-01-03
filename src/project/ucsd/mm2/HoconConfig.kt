68057d4b3eaf4eeef46bc194928002c6102edeb6
Chi Chow <cschow@ucsd.edu>
2021-01-03 15:48:28 -0800

    Add default endpoint and region for DynamoDB

--- src/project/ucsd/mm2/HoconConfig.kt
+++ src/project/ucsd/mm2/HoconConfig.kt
@@ -7,4 +7,8 @@ object HoconConfig : HoconApplicationConfig(ConfigFactory.load()) {
     operator fun get(key: String) = property(key).getString()
 
     val schedulerWorkers = this["scheduler.workers"].toInt()
+
+    val dbTableName = this["aws.dynamodb.table_name"]
+    val dbEndpoint = this["aws.dynamodb.endpoint"]
+    val dbRegion = this["aws.dynamodb.region"]
 }
